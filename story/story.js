3e5d1d6c884720ea526ad3150049cf65aa6fdcc6
Darkside8713 <101575966+Darkside8713@users.noreply.github.com>
2023-01-26 13:35:26 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -60,16 +60,18 @@ const tcontentString =
    '<div id="content">' +
     '<div id="siteNotice">' +
     "</div>" +
-    '<h1 id="firstHeading" class="firstHeading">Hamilton</h1>' +
+    '<h1 id="firstHeading" class="firstHeading">Trois-Rivière</h1>' +
     '<div id="bodyContent">' +
-    "<p><b>Hamilton</b> is a port city in the Canadian province of Ontario. " +
-    "Hamilton has a population of 569,353, and its census metropolitan area,  " +
-    "which includes Burlington and Grimsby, has a population of 785,184. " +
-    "he city is approximately 45 kilometres (28 mi) southwest of " +
-    "Toronto in the Greater Toronto and Hamilton Area (GTHA). " +
+    "<p><b>Trois-Rivière</b> is a city in the Mauricie administrative region of Quebec, Canada," +
+    "at the confluence of the Saint-Maurice and Saint Lawrence rivers," +
+    "on the north shore of the Saint Lawrence River across from the city of Bécancour." +
+    " It is part of the densely populated Quebec City–Windsor Corridor and is approximately" +
+    "halfway between Montreal and Quebec City. Trois-Rivières is the economic and cultural hub" +
+    "of the Mauricie region.[8] The settlement was founded by French colonists on July 4, 1634," +
+    "as the second permanent settlement in New France,[9] after Quebec City in 1608." +
     "</p>" +
-    '<p>Attribution: Hamilton, Ontario, <a href="https://en.wikipedia.org/wiki/Hamilton,_Ontario">' +
-    "https://en.wikipedia.org/wiki/Hamilton,_Ontario</a> "
+    '<p>Attribution: Hamilton, Ontario, <a href="https://en.wikipedia.org/wiki/Trois-Rivières">' +
+    "https://en.wikipedia.org/wiki/Trois-Rivières</a> "
     "</div>" +
     "</div>";
   
