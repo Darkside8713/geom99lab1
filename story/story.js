30fffa5002fc05eb49ece36ee747845e79d00749
Darkside8713 <101575966+Darkside8713@users.noreply.github.com>
2023-01-26 13:23:34 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -55,6 +55,41 @@ const hcontentString =
       map,
     });
   });
+
+const tcontentString =
+   '<div id="content">' +
+    '<div id="siteNotice">' +
+    "</div>" +
+    '<h1 id="firstHeading" class="firstHeading">Hamilton</h1>' +
+    '<div id="bodyContent">' +
+    "<p><b>Hamilton</b> is a port city in the Canadian province of Ontario. " +
+    "Hamilton has a population of 569,353, and its census metropolitan area,  " +
+    "which includes Burlington and Grimsby, has a population of 785,184. " +
+    "he city is approximately 45 kilometres (28 mi) southwest of " +
+    "Toronto in the Greater Toronto and Hamilton Area (GTHA). " +
+    "</p>" +
+    '<p>Attribution: Hamilton, Ontario, <a href="https://en.wikipedia.org/wiki/Hamilton,_Ontario">' +
+    "https://en.wikipedia.org/wiki/Hamilton,_Ontario</a> "
+    "</div>" +
+    "</div>";
+  
+  const tinfowindow = new google.maps.InfoWindow({
+    content: tcontentString,
+    ariaLabel: "Trois-Rivières",
+  });
+  
+  const tmarker = new google.maps.Marker({
+    position: { lat: 43.2557, lng: -79.8711 } ,
+    map,
+    title: "Trois-Rivières",
+  });
+
+  tmarker.addListener("click", () => {
+    tinfowindow.open({
+      anchor: tmarker,
+      map,
+    });
+  });
 }
 
 window.initMap = initMap;
