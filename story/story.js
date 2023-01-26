7e030f0ce7e2baf921eba168161ee8bc3da5961f
Darkside8713 <101575966+Darkside8713@users.noreply.github.com>
2023-01-26 13:08:24 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -28,20 +28,8 @@ const hcontentString =
     "</div>" +
     '<h1 id="firstHeading" class="firstHeading">Hamilton</h1>' +
     '<div id="bodyContent">' +
-    "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
-    "sandstone rock formation in the southern part of the " +
-    "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
-    "south west of the nearest large town, Alice Springs; 450&#160;km " +
-    "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
-    "features of the Uluru - Kata Tjuta National Park. Uluru is " +
-    "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
-    "Aboriginal people of the area. It has many springs, waterholes, " +
-    "rock caves and ancient paintings. Uluru is listed as a World " +
-    "Heritage Site.</p>" +
-    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-    "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
-    "(last visited June 22, 2009).</p>" +
-    "</div>" +
+    "<p><b>Hamilton</b> is a port city in the Canadian province of Ontario. Hamilton has a population of 569,353, and its census metropolitan area, which includes Burlington and Grimsby, has a population of 785,184. The city is approximately 45 kilometres (28 mi) southwest of Toronto in the Greater Toronto and Hamilton Area (GTHA).Conceived by George Hamilton when he purchased the Durand farm shortly after the War of 1812, the town of Hamilton became the centre of a densely populated and industrialized region at the west end of Lake Ontario known as the Golden Horseshoe.On January 1, 2001, the current boundaries of Hamilton were created through the amalgamation of the original city with other municipalities of the Regional Municipality of Hamilton–Wentworth.[8] Residents of the city are known as Hamiltonians. Traditionally, the local economy has been led by the steel and heavy manufacturing industries. During the 2010s, a shift toward the service sector occurred, such as health and sciences. Hamilton is home to the Royal Botanical Gardens, the Canadian Warplane Heritage Museum, the Bruce Trail, McMaster University, Mohawk College, and Redeemer University. McMaster University is ranked 4th in Canada and 69th in the world by Times Higher Education Rankings 2021.[10]</p>" '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/wiki/Hamilton,_Ontario"> </p>" 
+"</div>"
     "</div>";
   const hinfowindow = new google.maps.InfoWindow({
     content: hcontentString,
